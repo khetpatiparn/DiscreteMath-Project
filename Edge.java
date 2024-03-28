e203e1d6041b7d4b0355def70f35ee30eb812262
TTHAime <165275595+TTHAime@users.noreply.github.com>
2024-03-28 18:36:01 +0700

    Add files via upload

--- Edge.java
+++ Edge.java
@@ -1,59 +1,61 @@
-public class Edge {
-    private String name;
-    private int src, dst;
-    private int w;
+
+    public class Edge {
+        private String name;
+        private int src, dst;
+        private int w;
+        
+        public Edge(int src, int dst, int w){
+            this.src = src;
+            this.dst = dst;
+            this.w = w;
+        }
+        public Edge(int src, int dst){
+            this.src = src;
+            this.dst = dst;
+        }
+        
+        public Edge(String name, int src, int dst, int w){
+            this.name = name;
+            this.src = src;
+            this.dst = dst;
+            this.w = w;
+        }
     
-    public Edge(int src, int dst, int w){
-        this.src = src;
-        this.dst = dst;
-        this.w = w;
-    }
-    public Edge(int src, int dst){
-        this.src = src;
-        this.dst = dst;
-    }
+        public String getName() {
+            return name;
+        }
     
-    public Edge(String name, int src, int dst, int w){
-        this.name = name;
-        this.src = src;
-        this.dst = dst;
-        this.w = w;
+        public void setName(String name) {
+            this.name = name;
+        }
+    
+        public int getSrc() {
+            return src;
+        }
+    
+        public void setSrc(int src) {
+            this.src = src;
+        }
+    
+        public int getDst() {
+            return dst;
+        }
+    
+        public void setDst(int dst) {
+            this.dst = dst;
+        }
+    
+        public int getWeight() {
+            return w;
+        }
+    
+        public void setWeight(int w) {
+            this.w = w;
+        }
+        @Override
+        public String toString() {
+            return "Edge [name=" + name + ", src=" + src + ", dst=" + dst + ", w=" + w + "]";
+        }
+    
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getSrc() {
-        return src;
-    }
-
-    public void setSrc(int src) {
-        this.src = src;
-    }
-
-    public int getDst() {
-        return dst;
-    }
-
-    public void setDst(int dst) {
-        this.dst = dst;
-    }
-
-    public int getW() {
-        return w;
-    }
-
-    public void setW(int w) {
-        this.w = w;
-    }
-    @Override
-    public String toString() {
-        return "Edge [name=" + name + ", src=" + src + ", dst=" + dst + ", w=" + w + "]";
-    }
-
-}
