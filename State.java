0a01e877cde4d04b3b69d3cb90c72a60a47e18ab
PichsineeJi <165241739+PichsineeJi@users.noreply.github.com>
2024-03-29 11:43:15 +0700

    FNA - create each state as an object

---
+++ State.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class State {
+    private String name;
+    private boolean isFinal;
+    private List<Transition> transitions;
+
+    public State(String name) {
+        this.name = name;
+        this.isFinal = false;
+        this.transitions = new ArrayList<>();
+    }
+
+    public State(String name, boolean isFinal) {
+        this.name = name;
+        this.isFinal = isFinal;
+        this.transitions = new ArrayList<>();
+    }
+
+    public void addTransition(char symbol, State nextState) {
+        transitions.add(new Transition(symbol, nextState));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Transition getNextState(char symbol) {
+        for (Transition transition : transitions) {
+            if (transition.getSymbol() == symbol) {
+                return transition;
+            }
+        }
+        return null;
+    }
+
+    public boolean isFinal() {
+        return isFinal;
+    }
+}
