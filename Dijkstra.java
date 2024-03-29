346dd33efbba33234bde9ca1fa4f5fbacf0a2367
PichsineeJi <165241739+PichsineeJi@users.noreply.github.com>
2024-03-29 10:56:24 +0700

    Dijkstra algo

--- Dijkstra.java
+++ Dijkstra.java
@@ -1,7 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
 
-
 public class Dijkstra {
     public int nVer;
     public int[][] graph;
@@ -86,25 +85,5 @@ public class Dijkstra {
             }
         }
         return minIndex;
-    }
-
-    public static void main(String[] args) {
-        int V = 9;
-        int[][] graph = {
-            {0, 4, 0, 0, 0, 0, 0, 8, 0},
-            {4, 0, 8, 0, 0, 0, 0, 11, 0},
-            {0, 8, 0, 7, 0, 4, 0, 0, 2},
-            {0, 0, 7, 0, 9, 14, 0, 0, 0},
-            {0, 0, 0, 9, 0, 10, 0, 0, 0},
-            {0, 0, 4, 14, 10, 0, 2, 0, 0},
-            {0, 0, 0, 0, 0, 2, 0, 1, 6},
-            {8, 11, 0, 0, 0, 0, 1, 0, 7},
-            {0, 0, 2, 0, 0, 0, 6, 7, 0}
-        };
-
-        int source = 0;
-
-        Dijkstra dijkstra = new Dijkstra(V, graph, source);
-        dijkstra.dijkstra();
     }
 }
