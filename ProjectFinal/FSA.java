2af2d6e6d05dbb105120132d25e96a3b858d7450
TTHAime <165275595+TTHAime@users.noreply.github.com>
2024-03-29 17:14:02 +0700

    Add files via upload

--- ProjectFinal/FSA.java
+++ ProjectFinal/FSA.java
@@ -16,7 +16,6 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-
 public class FSA extends JPanel {
     private final int FRAME_WIDTH = 800;
     private final int FRAME_HEIGHT = 600;
@@ -31,58 +30,62 @@ public class FSA extends JPanel {
     JLabel outputLabel; // Added JLabel for displaying output
     private BufferedImage image;
 
-    public FSA(int input) {
+    public FSA() {
         setPreferredSize(FRAME_SIZE); // set Size of Title Game Screen
         setLayout(null); // use absolute layout
         setBackground(Color.WHITE);
+        int labelXaxis = 565;
+        int labelYaxis = 120;
 
         // Label above JComboBox
         selectStartStateLabel = new JLabel("Select Start State");
-        selectStartStateLabel.setBounds(565, 180, 200, 30);
+        selectStartStateLabel.setBounds(labelXaxis, labelYaxis, 200, 30);
         selectStartStateLabel.setForeground(Color.WHITE);
         selectStartStateLabel.setFont(new Font("Arial", Font.PLAIN, 25));
         add(selectStartStateLabel);
 
         // JComboBox
-        Integer[] items = new Integer[input];
-        for (int i = 0; i < input; i++) {
+        Integer[] items = new Integer[3];
+        for (int i = 0; i < 3; i++) {
             items[i] = i;
         }
         comboBox = new JComboBox<>(items);
-        comboBox.setBounds(565, 230, 200, 30);
+        comboBox.setBounds(labelXaxis, labelYaxis + 50, 200, 30);
         add(comboBox);
 
         // Separator Label
         separatorLabel = new JLabel("Input : 0,1");
-        separatorLabel.setBounds(565, 290, 200, 30);
+        separatorLabel.setBounds(labelXaxis, labelYaxis + 100, 200, 30);
         separatorLabel.setFont(new Font("Arial", Font.PLAIN, 25));
         separatorLabel.setForeground(Color.WHITE);
         add(separatorLabel);
 
         // JTextField
         textField = new JTextField();
-        textField.setBounds(565, 320, 200, 30);
+        textField.setBounds(labelXaxis, labelYaxis + 150, 200, 30);
         textField.setFont(new Font("Arial", Font.PLAIN, 20));
         textField.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
                 char c = e.getKeyChar();
                 if (!(c == '0' || c == '1')) {
-                    e.consume(); //filter input 
+                    e.consume(); // filter input
                 }
             }
 
             @Override
-            public void keyPressed(KeyEvent e) {}
+            public void keyPressed(KeyEvent e) {
+            }
 
             @Override
-            public void keyReleased(KeyEvent e) {}
+            public void keyReleased(KeyEvent e) {
+            }
         });
         add(textField);
 
         // Submit Button
         submitButton = new JButton("Submit");
-        submitButton.setBounds(565, 360, 95, 30);
+        submitButton.setBounds(labelXaxis, labelYaxis + 190, 95, 30);
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -96,7 +99,7 @@ public class FSA extends JPanel {
 
         // Clear Button
         clearButton = new JButton("Clear");
-        clearButton.setBounds(670, 360, 95, 30);
+        clearButton.setBounds(labelXaxis + 105, labelYaxis + 190, 95, 30);
         clearButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -107,7 +110,7 @@ public class FSA extends JPanel {
 
         // Exit Button
         exitButton = new JButton("Exit");
-        exitButton.setBounds(565, 500, 200, 30); // Adjusted position
+        exitButton.setBounds(labelXaxis, 500, 200, 30); // Adjusted position
         add(exitButton);
 
         try {
