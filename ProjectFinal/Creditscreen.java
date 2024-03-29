d814f73c8deaf81906b423de866d18e2a57b7455
TTHAime <165275595+TTHAime@users.noreply.github.com>
2024-03-29 10:58:22 +0700

    Add files via upload

--- ProjectFinal/Creditscreen.java
+++ ProjectFinal/Creditscreen.java
@@ -1,52 +1,59 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
-public class Creditscreen extends JPanel{
+public class Creditscreen extends JPanel {
     JLabel creditsTitle, name1, name2, name3;
-    JLabel CbackBtn;
+    JButton CbackBtn;
     private final int FRAME_WIDTH = 800;
     private final int FRAME_HEIGHT = 600;
     private final Dimension FRAME_SIZE = new Dimension(FRAME_WIDTH, FRAME_HEIGHT);
-    
-    public Creditscreen(){
+
+    public Creditscreen() {
         setPreferredSize(FRAME_SIZE); // set Size of Title Game Screen
         setLayout(null); // use absolute layout
         setBackground(Color.WHITE);
-        
+
         // creditsTitle
         creditsTitle = new JLabel();
         creditsTitle.setText("CREDITS");
-        creditsTitle.setBounds(190, 60, 454, 104);
+        creditsTitle.setBounds(313, 20, 454, 104);
+        creditsTitle.setFont(new Font("Arial", Font.PLAIN, 40));
         add(creditsTitle);
 
         // name
         name1 = new JLabel();
-        name1.setText("66050253  PATIPARN LAKORN");
-        name1.setBounds(60, 200, 450, 80);
+        name1.setText("66050176  THANAPHON CHAWDONG");
+        name1.setBounds(223, 150, 500, 80);
+        name1.setForeground(Color.black);
+        name1.setFont(new Font("Arial", Font.PLAIN, 20));
         add(name1);
 
         name2 = new JLabel();
-        name2.setText("66050635  CHATCHAI PANYANAI");
-        name2.setBounds(60, 300, 490, 80);
+        name2.setText("66050253  PATIPARN LAKORN");
+        name2.setBounds(223, 200, 490, 80);
         name2.setForeground(Color.black);
+        name2.setFont(new Font("Arial", Font.PLAIN, 20));
         add(name2);
 
         name3 = new JLabel();
-        name3.setText("66050752  TANAKORN KAMANA");
-        name3.setBounds(60, 400, 490, 80);
+        name3.setText("66050301  Pichsinee Jirawattanakasem");
+        name3.setBounds(223, 250, 490, 80);
+        name3.setForeground(Color.black);
+        name3.setFont(new Font("Arial", Font.PLAIN, 20));
         add(name3);
-        
-        // BackButton (But use Label)
-        CbackBtn = new JLabel();
+
+        // BackButton ใช้ JButton แทน JLabel
+        CbackBtn = new JButton(); // สร้าง JButton
         CbackBtn.setText("BACK");
-        CbackBtn.setBounds(213, 306, 150, 50);
+        CbackBtn.setBounds(320, 370, 150, 50);
         CbackBtn.setOpaque(true);
-        CbackBtn.setBackground(new Color(166, 166, 166).darker().darker());
-        CbackBtn.setHorizontalAlignment(SwingConstants.CENTER);
-        CbackBtn.setVerticalAlignment(SwingConstants.CENTER);
+        CbackBtn.setBackground(new Color(166, 166, 166));
         add(CbackBtn);
     }
 }
--- ProjectFinal/Matrix.java
+++ ProjectFinal/Matrix.java
@@ -9,18 +9,18 @@ public class Matrix extends JPanel {
     private final int FRAME_WIDTH = 800;
     private final int FRAME_HEIGHT = 600;
     private final Dimension FRAME_SIZE = new Dimension(FRAME_WIDTH, FRAME_HEIGHT);
-    JLabel Head;
+    JLabel matrix_title;
 
     public Matrix() {
         setPreferredSize(FRAME_SIZE); // set Size of Title Game Screen
         setLayout(null); // use absolute layout
         setBackground(Color.WHITE);
-        Head = new JLabel();
-        Head.setText("Matrix");
-        Head.setBounds(335, 20, 590, 60);
-        Head.setOpaque(false);
-        Head.setFont(new Font("Arial", Font.PLAIN, 40));
-        Head.setForeground(Color.BLACK);
-        add(Head);
+        matrix_title = new JLabel();
+        matrix_title.setText("Adjacency Matrix");
+        matrix_title.setBounds(242, 20, 590, 60);
+        matrix_title.setOpaque(false);
+        matrix_title.setFont(new Font("Arial", Font.PLAIN, 40));
+        matrix_title.setForeground(Color.BLACK);
+        add(matrix_title);
     }
 }
