15ac71c7cc1c76eac41e369967b2debdffeaae1d
PichsineeJi <165241739+PichsineeJi@users.noreply.github.com>
2024-03-29 18:29:32 +0700

    FNA - check ans

---
+++ ProjectFinal/checkFiniteAuto.java
@@ -0,0 +1,19 @@
+public class checkFiniteAuto {
+    private State currentState;
+
+    public void defineAutomata(State initialState) {
+        currentState = initialState;
+    }
+
+    public boolean isInputAccepted(String input) {
+        for (char ch : input.toCharArray()) {
+            Transition nextTransition = currentState.getNextState(ch);
+            if (nextTransition == null) {
+                return false;
+            }
+            System.out.println("Transition: " + currentState.getName() + " -> " + nextTransition.getNextState().getName() + " on symbol '" + ch + "'");
+            currentState = nextTransition.getNextState();
+        }
+        return currentState.isFinal();
+    }
+}
