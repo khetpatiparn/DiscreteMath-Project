0d63bb612bd8bcb52273665288279096fe8f5e86
PichsineeJi <165241739+PichsineeJi@users.noreply.github.com>
2024-03-29 11:46:34 +0700

    FNA - keep transition for each state

---
+++ Transition.java
@@ -0,0 +1,18 @@
+public class Transition {
+    private char symbol;
+    private State nextState;
+
+    public Transition(char symbol, State nextState) {
+        this.symbol = symbol;
+        this.nextState = nextState;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public State getNextState() {
+        return nextState;
+    }
+
+}
