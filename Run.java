92ca150a0e8154ff9f6716b10702e2b311769b87
khetpatiparn <patiparn6mgup@gmail.com>
2024-04-08 08:45:22 +0700

    del EdgePos and VertexPos files

---
+++ Run.java
@@ -0,0 +1,14 @@
+import javax.swing.SwingUtilities;
+
+public class Run {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+        @Override
+        public void run() {
+            new StartFrame();
+        }
+        });
+    }
+        
+}
+    
